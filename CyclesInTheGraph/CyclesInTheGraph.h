fe273f11553ec34213b9d50222b6f91a61ebec5e
Екатерина Микулина <katushka.mikulina@gmail.com>
2023-04-08 16:05:06 +0300

    Added headers documentation

--- CyclesInTheGraph/CyclesInTheGraph.h
+++ CyclesInTheGraph/CyclesInTheGraph.h
@@ -27,20 +27,52 @@ private:
 };
 
 
+/*! Получить правильное имя цикла. Правильное имя состоит из строки, полученной при
+	конкатенации имен каждой вершины, и сортировки полученной строки в алфавитном порядке.
+* \param[in] cycle - цикл, для которого необходимо получить правильное имя.
+* \return правильное имя цикла.
+*/
 std::string getCorrectCycleName(const std::vector<Vertex*>& cycle);
 
 
+/*! Определить наличие цикла для указанной вершины, используя алгоритм поиска в глубину.
+* \param[in] viewedVertex - рассматриваемая вершина.
+* \param[in, out] recStack - массив для промежуточного хранения цикла.
+* // TODO убарть \param[in, out] visited - массив для хранения просмотренных вершин. std::set<Vertex*> visited
+* \param[in] cycles - массив с найденными циклами.
+* \return 0 - если цикл не найден. 1 - если цикл найден.
+*/
 bool dfs(Vertex* viewedVertex, std::vector<Vertex*>& recStack, std::set<Vertex*> visited, 
 	const std::map<std::string, std::vector<Vertex*>>& cycles);
 
 
+/*! Поиск всех уникальных циклов в графе.
+* \param[in] graph - массив с вершинами (граф), в котором идет поиск.
+* \param[out] cycles - массив с найденными циклами.
+* \return 0 - если цикл не найден. 1 - если цикл найден.
+*/
 bool findCycles(const std::vector<Vertex*>& graph, std::map<std::string, std::vector<Vertex*>>& cycles);
 
 
+/*! Найти первую вершину с указанным названием в графе.
+* \param[in] vertexName - название искомой вершины.
+* \param[out] graph - массив с вершинами (граф), в котором идет поиск.
+* return найденная вершина или нулевой указатель
+*/
 Vertex* findVertexInGraph(const std::string& vertexName, const std::vector<Vertex*>& graph);
 
 
+/*! Удалить перечисленные символы и слева и справа от строки.
+* \param[in, out] str строка из которой удаляют крайние правые и левые символы.
+* \param[in] stripBy список удаляемых символов справа от строки.
+*/
 void strip(std::string& str, const char* stripBy = " \t\n\v\f\r");
 
 
+/*! Прочитывает входные данные из CSV файла, на их основе
+	создает вершины Vertex и сохраняет их в массив graph.
+* \param[in] input_file_path - строка с путем к файлу с входными данными.
+* \param[out] graph - массив с вершинами.
+* \param[out] delimiter - разделитель элементов в строке, использующийся в CSV файле.
+*/
 void readCSVFile(const std::string& input_file_path, std::vector<Vertex*>& graph, const char delimiter = ';');
