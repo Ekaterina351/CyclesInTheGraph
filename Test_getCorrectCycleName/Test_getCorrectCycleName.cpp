286684e07deeff27e5743ee51334f9742ecba5de
Екатерина Микулина <katushka.mikulina@gmail.com>
2023-05-09 10:06:21 +0300

    Added sample unit test of getCorrectCycleName function

---
+++ Test_getCorrectCycleName/Test_getCorrectCycleName.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CyclesInTheGraph/CyclesInTheGraph.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestgetCorrectCycleName
+{
+	TEST_CLASS(TestgetCorrectCycleName)
+	{
+	public:
+		
+		TEST_METHOD(sampleTest)
+		{
+			std::vector<Vertex*> input_cycle = {
+				new Vertex("x3"),
+				new Vertex("x2"),
+				new Vertex("x3")
+			};
+			std::string exp_name = "233xxx";
+
+			std::string res_name = getCorrectCycleName(input_cycle);
+			Assert::AreEqual(exp_name, res_name);
+		}
+	};
+}
