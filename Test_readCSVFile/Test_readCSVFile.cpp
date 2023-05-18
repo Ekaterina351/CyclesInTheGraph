3e96bdbda8ddb232e6cd3b9d1195e7ebc4694536
Екатерина Микулина <katushka.mikulina@gmail.com>
2023-05-18 21:21:26 +0300

    Added unit test for readCSVFile function

--- Test_readCSVFile/Test_readCSVFile.cpp
+++ Test_readCSVFile/Test_readCSVFile.cpp
@@ -59,5 +59,387 @@ namespace TestreadCSVFile
 
 			compareGraphs(exp_graph, res_graph);
 		}
+		TEST_METHOD(noRowsAndColumnsInCSVFile)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\noRowsAndColumnsInCSVFile.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(oneRowInCSVFile)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\oneRowInCSVFile.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(oneColumnInCSVFile)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\oneColumnInCSVFile.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(moreColumnsThanRowsInTable)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\moreColumnsThanRowsInTable.csv";
+
+			Vertex* v1 = new Vertex("X1");
+			Vertex* v2 = new Vertex("X2");
+			Vertex* v3 = new Vertex("X3");
+			v1->adjacentVertices = { v2, v3 };
+			v2->adjacentVertices = { v1, v3 };
+
+			std::vector<Vertex*> exp_graph = {
+				v1, v2, v3
+			};
+			std::vector<Vertex*>res_graph;
+			readCSVFile(filePath.string(), res_graph);
+
+			compareGraphs(exp_graph, res_graph);
+		}
+		TEST_METHOD(fewerColumnsThanRowsInTable)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\fewerColumnsThanRowsInTable.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(unnamedVertexInHorizontalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\unnamedVertexInHorizontalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(unnamedVertexInVerticalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\unnamedVertexInVerticalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(vertexWithWhitespaceNameInHorizontalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\vertexWithWhitespaceNameInHorizontalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(vertexWithWhitespaceNameInVerticalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\vertexWithWhitespaceNameInVerticalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(whitespaceBeforeVertexName)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\whitespaceBeforeVertexName.csv";
+
+			Vertex* v1 = new Vertex("X1");
+			Vertex* v2 = new Vertex("X2");
+			Vertex* v3 = new Vertex("X3");
+			v1->adjacentVertices = { v2, v3 };
+			v2->adjacentVertices = { v1, v3 };
+			v3->adjacentVertices = { v1, v2 };
+			std::vector<Vertex*> exp_graph = {
+				v1, v2, v3
+			};
+			std::vector<Vertex*>res_graph;
+			readCSVFile(filePath.string(), res_graph);
+
+			compareGraphs(exp_graph, res_graph);
+		}
+		TEST_METHOD(whitespaceAfterVertexName)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\whitespaceAfterVertexName.csv";
+
+			Vertex* v1 = new Vertex("X1");
+			Vertex* v2 = new Vertex("X2");
+			Vertex* v3 = new Vertex("X3");
+			v1->adjacentVertices = { v2, v3 };
+			v2->adjacentVertices = { v1, v3 };
+			v3->adjacentVertices = { v1, v2 };
+
+			std::vector<Vertex*> exp_graph = {
+				v1, v2, v3
+			};
+			std::vector<Vertex*>res_graph;
+			readCSVFile(filePath.string(), res_graph);
+
+			compareGraphs(exp_graph, res_graph);
+		}
+		TEST_METHOD(whitespaceInVertexName)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\whitespaceInVertexName.csv";
+
+			Vertex* v1 = new Vertex("X\\t1");
+			Vertex* v2 = new Vertex("X2");
+			Vertex* v3 = new Vertex("X3");
+			v1->adjacentVertices = { v2, v3 };
+			v2->adjacentVertices = { v1, v3 };
+			v3->adjacentVertices = { v1, v2 };
+
+			std::vector<Vertex*> exp_graph = {
+				v1, v2, v3
+			};
+			std::vector<Vertex*>res_graph;
+			readCSVFile(filePath.string(), res_graph);
+
+			compareGraphs(exp_graph, res_graph);
+		}
+		TEST_METHOD(whitespaceBeforeAndAfterVertexName)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\whitespaceBeforeAndAfterVertexName.csv";
+
+			Vertex* v1 = new Vertex("X1");
+			Vertex* v2 = new Vertex("X2");
+			Vertex* v3 = new Vertex("X3");
+			v1->adjacentVertices = { v2, v3 };
+			v2->adjacentVertices = { v1, v3 };
+			v3->adjacentVertices = { v1, v2 };
+
+			std::vector<Vertex*> exp_graph = {
+				v1, v2, v3
+			};
+			std::vector<Vertex*>res_graph;
+			readCSVFile(filePath.string(), res_graph);
+
+			compareGraphs(exp_graph, res_graph);
+		}
+		TEST_METHOD(whitespaceBeforeAfterAndInVertexName)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\whitespaceBeforeAfterAndInVertexName.csv";
+
+			Vertex* v1 = new Vertex("X    1");
+			Vertex* v2 = new Vertex("X2");
+			Vertex* v3 = new Vertex("X3");
+			v1->adjacentVertices = { v2, v3 };
+			v2->adjacentVertices = { v1, v3 };
+			v3->adjacentVertices = { v1, v2 };
+
+			std::vector<Vertex*> exp_graph = {
+				v1, v2, v3
+			};
+			std::vector<Vertex*>res_graph;
+			readCSVFile(filePath.string(), res_graph);
+
+			compareGraphs(exp_graph, res_graph);
+		}
+		TEST_METHOD(duplicateVerticesInHorizontalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\duplicateVerticesInHorizontalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(duplicateVerticesInVerticalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\duplicateVerticesInVerticalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(mismatchedOrderOfVerticesBetweenHeaders)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\mismatchedOrderOfVerticesBetweenHeaders.csv";
+
+			Vertex* v1 = new Vertex("X1");
+			Vertex* v2 = new Vertex("X2");
+			Vertex* v3 = new Vertex("X3");
+			v1->adjacentVertices = { v2, v3 };
+			v2->adjacentVertices = { v1, v3 };
+			v3->adjacentVertices = { v1, v2 };
+			std::vector<Vertex*> exp_graph = {
+				v1, v2, v3
+			};
+			std::vector<Vertex*>res_graph;
+			readCSVFile(filePath.string(), res_graph);
+
+			compareGraphs(exp_graph, res_graph);
+		}
+		TEST_METHOD(emptyCellsInTableBody)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\emptyCellsInTableBody.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(cellsWithWhitespaceInTableBody)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\cellsWithWhitespaceInTableBody.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(cellsWithNonBinaryTextInTableBody)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\cellsWithNonBinaryTextInTableBody.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(cyclesInGraphDefinedInTableBody)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\cyclesInGraphDefinedInTableBody.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(tableBodyWidthGreaterThanHorizontalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\tableBodyWidthGreaterThanHorizontalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(tableBodyWidthLessThanHorizontalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\tableBodyWidthLessThanHorizontalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(tableBodyHeightGreaterThanVerticalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\tableBodyHeightGreaterThanVerticalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
+		TEST_METHOD(tableBodyHeightLessThanVerticalHeader)
+		{
+			std::filesystem::path filePath = currentPath / "Test_readCSVFile\\input_files\\tableBodyHeightLessThanVerticalHeader.csv";
+
+			bool exceptionThrown = false;
+			try {
+				std::vector<Vertex*>res_graph;
+				readCSVFile(filePath.string(), res_graph);
+			}
+			catch (const InvalidGraphAssignment&) {
+				exceptionThrown = true;
+			}
+			Assert::IsTrue(exceptionThrown, L"Expected InvalidGraphAssignment to be thrown.");
+		}
 	};
 }
